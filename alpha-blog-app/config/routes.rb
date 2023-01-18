c859b5a87e0296b009e7fd873c84085526fef2ea
Justice Arthur <justicea83@gmail.com>
2023-01-18 12:22:39 +0000

    add users edit

--- alpha-blog-app/config/routes.rb
+++ alpha-blog-app/config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   root "pages#main"
   resources :articles
   get 'signup', to: 'users#new'
-  post 'users', to: 'users#create'
+  resources :users, except: [:new]
 end
