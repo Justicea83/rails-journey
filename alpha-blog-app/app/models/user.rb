a78c688e5212c110c677bdff052d9983e17e6143
Justice Arthur <justicea83@gmail.com>
2023-01-18 11:50:33 +0000

    add user sign up form

--- alpha-blog-app/app/models/user.rb
+++ alpha-blog-app/app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     before_save { self.email = email.downcase }
     validates :username, presence: true, length: {minimum: 3, maximum: 25}, uniqueness: {case_sensitive: false}
+    has_secure_password
 
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
