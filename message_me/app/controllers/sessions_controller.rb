aa7a17b6de270e7f4119278b58f16330b4a45564
Justice Arthur <justicea83@gmail.com>
2023-01-20 00:20:40 +0000

    implement login

--- message_me/app/controllers/sessions_controller.rb
+++ message_me/app/controllers/sessions_controller.rb
@@ -1,4 +1,23 @@
 class SessionsController < ApplicationController
     def index
     end
+
+    def create
+        user = User.find_by(username: params[:session][:username])
+
+        if user && user.authenticate(params[:session][:password])
+            session[:user_id] = user.id
+            flash[:success] = "You have successfully logged in"
+            redirect_to root_path
+        else
+            flash.now[:error] = "Your credentials do not match our records"
+            render 'new'
+        end
+    end
+
+    def destroy
+        session[:user_id] = null
+        flash[:success] = "You have successfully logged out!"
+        redirect_to login_path
+    end
 end
