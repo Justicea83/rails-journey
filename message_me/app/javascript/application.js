8a90a6578c7322d30047f68e13ca754ad57baf73
Justice Arthur <justicea83@gmail.com>
2023-01-20 01:55:47 +0000

    added auto scroll

--- message_me/app/javascript/application.js
+++ message_me/app/javascript/application.js
@@ -3,11 +3,21 @@ import "@hotwired/turbo-rails"
 import * as jquery from "jquery"
 import "semantic-ui"
 import "controllers"
+import "channels"
 
+
+window.scroll_bottom = function () {
+    const messagesContainer = $('#messages');
+    if (messagesContainer.length > 0) {
+        messagesContainer.scrollTop(messagesContainer[0].scrollHeight)
+    }
+}
 $(document).on('turbo:load', function () {
+    scroll_bottom()
     $('.ui.dropdown').dropdown();
 
     $('.message .close').on('click', function () {
         $(this).closest('.message').transition('fade');
     });
-});import "channels"
+
+});
